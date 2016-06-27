4d540d0eda8b6c6b4ab51a3b4f5726ad0350f2fe
Cristian Bernal <crisbera@gmail.com>
2016-06-27 11:23:35 -0500

    Repositorio inicial

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Aplicación</title>
+</head>
+<body>
+	<h1>Página principal</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione aut nulla labore molestias animi perspiciatis, et ea qui tenetur vitae inventore ab laboriosam sunt quasi est fugiat ex amet accusantium!</p>
+	<h2>Encabezado tipo H2</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium necessitatibus ipsum delectus dolores, soluta perspiciatis placeat ullam quis ex eos, dolorem dicta ea assumenda enim tenetur est beatae nobis non.</p>
+</body>
+</html>
